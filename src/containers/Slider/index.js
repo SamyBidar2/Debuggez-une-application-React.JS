ec75aa330b14a3884ce6edeea8f6d6552d752b68
SamyBidar2 <samybidarpro@gmail.com>
2024-08-01 16:14:50 +0200

    Correction du Warning sur le Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,58 +7,65 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+  const [timeoutId, setTimeoutId] = useState(null);
   const byDateDesc = data?.focus?.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(byDateDesc && (index < (byDateDesc.length)-1) ? index + 1 : 0),
+    const id = setTimeout(
+      () => setIndex(byDateDesc && index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
+    setTimeoutId(id);
   };
-  
+
   useEffect(() => {
     nextCard();
-  });
 
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-          <>
-          <div
-            key={`${event.title}-${event.id}`} // key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+    // Ajout de Clear timeout
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [index, byDateDesc]);
+
+return (
+  <div className="SlideCardList">
+    {byDateDesc?.map((event, idx) => (
+      <div key={`${event.title}-${event.id}`}>
+        <div
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-            {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`radio-${event.idx}`} 
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  onChange={() => setIndex(radioIdx)}
-                />
-              ))}
-            </div>    
+        </div>
+        <div className="SlideCard__paginationContainer">
+          <div className="SlideCard__pagination">
+            {byDateDesc.map((e, radioIdx) => (
+              <input
+                key={`radio-${e.title}`}
+                type="radio"
+                name="radio-button"
+                checked={index === radioIdx}
+                onChange={() => setIndex(radioIdx)}
+              />
+            ))}
           </div>
-          </>
-        ))}
-    </div>
-    );
+        </div>
+      </div>
+    ))}
+  </div>
+);
 };
 
 export default Slider;
